e4f988d0ea555887984560752db55e2e2a894477
Ko_n P <tnk.ppj6@gmail.com>
2018-06-11 16:18:23 +0700

    'finishEx1'

--- ex1/answer.sql
+++ ex1/answer.sql
@@ -17,17 +17,28 @@ select count(*) from products where price >= 180;
 select name, price from products where price >= 180 order by price desc, name asc;
 
 --Select all the data from the products, including all the data for each product's manufacturer.
---select p.* m.name from products p, Manufacturers m;
+select p.*, m.name from products p,manufacturers m where p.manufacturer=m.code;
 
 --Select the product name, price, and manufacturer name of all the products.
-
+select p.name, p.price, m.name from products p, manufacturers m where p.manufacturer=m.code;
 
 --Select the average price of each manufacturer's products, showing the manufacturer's name.
+select avg(price),m.name from products p, manufacturers m where p.manufacturer=m.code group by manufacturer;
+
 --Select the names of manufacturer whose products have an average price larger than or equal to $150.
+select name from manufacturers where code in (select manufacturer from products group by manufacturer having avg(price)>=150);
+
 --Select the name and price of the cheapest product.
+select name, price from products where price = (select min(price) from products);
+
 --Select the name of each manufacturer along with the name and price of its most expensive product.
+select name from manufacturers where code=(select manufacturer from products where price=(select max(price) from products));
+
 --Add a new product: Loudspeakers, $70, manufacturer 2.
+INSERT INTO Products(Name,Price,Manufacturer) VALUES('Loudspeakers',70,2);
+
 --Update the name of product 8 to "Laser Printer".
+update products set name = 'Laser Printer' where code = 8;
 
 
 
